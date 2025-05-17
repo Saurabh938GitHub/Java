aeac0c3e2627aa11d2f0639985daafba7b69606d
SaurabhKumar <938saurav@gmail.com>
2025-05-17 13:57:35 +0530

    git squash

--- javaPratice/src/javaPratice/arraySortingNotString.java
+++ javaPratice/src/javaPratice/arraySortingNotString.java
@@ -38,8 +38,6 @@ public class arraySortingNotString {
     
 	System.out.println(intList.stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList()));
 	System.out.println(intList.stream().sorted(Comparator.naturalOrder()).collect(Collectors.toList()));
-	System.out.println("2");
-	System.out.println("2");
 	}
 	
 
