e7329551befceca104b19ddff5e98d419d3bb06e
SaurabhKumar <938saurav@gmail.com>
2025-05-17 13:37:10 +0530

    11

--- javaPratice/src/javaPratice/arraySortingNotString.java
+++ javaPratice/src/javaPratice/arraySortingNotString.java
@@ -38,8 +38,6 @@ public class arraySortingNotString {
     
 	System.out.println(intList.stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList()));
 	System.out.println(intList.stream().sorted(Comparator.naturalOrder()).collect(Collectors.toList()));
-    System.out.println("333");
-    System.out.println("444");
 
 	}
 	
