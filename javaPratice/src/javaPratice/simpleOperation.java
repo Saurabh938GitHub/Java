1accab5f8e45f9010c2a2e34602c2dbef08a4091
SaurabhKumar <938saurav@gmail.com>
2025-05-17 13:27:55 +0530

    1

--- javaPratice/src/javaPratice/simpleOperation.java
+++ javaPratice/src/javaPratice/simpleOperation.java
@@ -40,6 +40,8 @@ public class simpleOperation {
 	System.out.println(intList.stream().sorted(Comparator.naturalOrder()).collect(Collectors.toList()));
 System.out.println("111");
 System.out.println("222");
+System.out.println("333");
+System.out.println("444");
 	}
 
 }
