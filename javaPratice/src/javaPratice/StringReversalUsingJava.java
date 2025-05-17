b4f9b7feb9265138332ac2b4f659159608a5818d
saurabh938569 <85559158+saurabh938569@users.noreply.github.com>
2025-05-17 14:38:06 +0530

    added program to reverse string using pure java (#10)

---
+++ javaPratice/src/javaPratice/StringReversalUsingJava.java
@@ -0,0 +1,22 @@
+package javaPratice;
+
+import java.util.Scanner;
+
+public class StringReversalUsingJava {
+	public static void main(String[] args) {
+
+	    Scanner obj= new Scanner(System.in);
+	    System.out.println("enter the string");
+	    String name= obj.next();
+	    System.out.println("entered string is: "+name);
+	    
+	    char[] c=new char[name.length()];
+	    
+	    for(int i=0; i<name.length();i++) {
+	    	c[name.length()-i-1]=name.charAt(i);
+	    }
+	    obj.close();
+	    System.out.println("reversed string is: "+new String(c));
+
+}
+}
