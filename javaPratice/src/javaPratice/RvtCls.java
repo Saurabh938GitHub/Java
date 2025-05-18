bf283bc45cdd470b105621a18dfd515d4ed12bd5
SaurabhKumar <938saurav@gmail.com>
2025-05-18 12:00:31 +0530

    revert operation 1

---
+++ javaPratice/src/javaPratice/RvtCls.java
@@ -0,0 +1,11 @@
+package javaPratice;
+
+public class RvtCls {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("added class to perform revert operation");
+
+	}
+
+}
