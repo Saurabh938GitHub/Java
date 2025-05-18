363aaf732532a3f6b00dd0fbd8da652ddb07a668
SaurabhKumar <938saurav@gmail.com>
2025-05-18 12:36:09 +0530

    reset op1

---
+++ javaPratice/src/javaPratice/gitResetOp.java
@@ -0,0 +1,11 @@
+package javaPratice;
+
+public class gitResetOp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("git reset op");
+
+	}
+
+}
